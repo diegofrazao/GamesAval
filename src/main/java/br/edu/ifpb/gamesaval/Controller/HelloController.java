eb8262a0c3ed930eba49b6d42ed0c70a868dd883
Diego Frazão <diiegofrazao10@gmail.com>
2022-04-21 18:47:29 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/gamesaval/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.gamesaval.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("/")
+    public String getHello() {
+        return "/HelloView/hello";
+    }
+}
---
+++ src/main/java/br/edu/ifpb/gamesaval/GamesAvalApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.gamesaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesAvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesAvalApplication.class, args);
+    }
+
+}
